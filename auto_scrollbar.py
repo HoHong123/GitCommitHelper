a419f480229f88e9c3dadfaddfe27baa809741f7
JasonCatze <epzmfhs@naver.com>
2025-06-21 06:36:38 +0900

    [Feat] 🆕 21.06.25 Add 'auto_scrollbar.py'

---
+++ auto_scrollbar.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class AutoScrollbar(tk.Scrollbar):
+    def set(self, lo, hi):
+        if float(lo) <= 0.0 and float(hi) >= 1.0:
+            self.grid_remove()
+        else:
+            self.grid()
+        super().set(lo, hi)
+
+    def pack(self, **kw): pass
+    def place(self, **kw): pass
